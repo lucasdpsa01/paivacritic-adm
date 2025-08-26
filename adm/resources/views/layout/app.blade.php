39a3514a5902b63c80947ec83d2c63adc0e99be4
lucasdpsa01 <lucasdsa2801@gmail.com>
2025-08-25 23:17:05 -0300

    falta o rating e o message

---
+++ adm/resources/views/layout/app.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PCADM</title>
+</head>
+<body>
+    <header>
+
+    </header>
+    <main>@yield('form')</main>
+    <footer>
+        
+    </footer>
+</body>
+</html>

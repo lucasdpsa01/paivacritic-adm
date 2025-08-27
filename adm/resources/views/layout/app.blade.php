d571de53d881d539e18d3ca9e443f3bbd01c8e75
lucasdpsa01 <lucasdsa2801@gmail.com>
2025-08-27 17:04:13 -0300

    90% view feito

--- adm/resources/views/layout/app.blade.php
+++ adm/resources/views/layout/app.blade.php
@@ -9,16 +9,18 @@
     <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <link rel="stylesheet" href="/css/style.css">
 </head>
-
 <body>
     <header>
-        <h1>paivacritic Admin</h1>
+        <img src="/img/engrenagem.png" alt="engrenagem-icon" height="40">
+        <h1>paivacritic admin</h1>
     </header>
     <main>@yield('form')</main>
     <footer>
-
+        &copy;
+        <p>2025</p>
+        <p>paivacritic adm</p>
     </footer>
 </body>
-
 </html>
--- adm/resources/views/welcome.blade.php
+++ adm/resources/views/welcome.blade.php
@@ -3,27 +3,37 @@
 @section('form')
 <form method="POST" enctype="multipart/form-data">
     @csrf
-    <div class="mb-3">
+    <div class="mb-3" id="divForm">
         <label for="inputName" class="form-label">Nome do Jogo</label>
         <input type="text" class="form-control" id="inputName">
     </div>
-    <div id="mb-3">
+    <div class="mb-3" id="divForm">
         <label for="formFile" class="form-label">Imagem</label>
-        <input class="form-control form-control-sm" type="file" name="foto" id="formFile">
+        <input class="form-control form-control" type="file" name="foto" id="formFile">
     </div>
-    <div id="mb-3">
+    <div class="mb-3" id="divForm">
+        <label class="form-label">Status</label>
         <select class="form-select" aria-label="Default select example">
-            <option selected>Open this select menu</option>
+            <option selected>Selecione o status</option>
             <option value="1">Concluido</option>
             <option value="2">Jogando</option>
             <option value="3">Em breve</option>
         </select>
     </div>
-    <div class="mb-3">
-        <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
+    <div class="mb-3" id="divForm">
+        <label class="form-label">Qual é sua nota?</label>
+        <select class="form-select" aria-label="Default select example">
+            <option selected>Selecione o status</option>
+            <option value="1">Muito bom</option>
+            <option value="2">Mais ou Menos</option>
+            <option value="3">Ruim</option>
+        </select>
+    </div>
+    <div class="mb-3" id="divForm">
+        <label for="exampleFormControlTextarea1" class="form-label">Mensagem</label>
         <textarea class="form-control form-control-sm" id="exampleFormControlTextarea1" rows="3"></textarea>
     </div>
     
-    <button type="submit" class="btn btn-primary">Adicionar Jogo</button>
+    <button type="submit" class="btn btn-primary" id="btn">Adicionar Jogo</button>
 </form>
 @endsection

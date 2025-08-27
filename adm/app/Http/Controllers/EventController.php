6cb3afc8d9cb2303cefd0f108efa071dbc6444d9
lucasdpsa01 <lucasdsa2801@gmail.com>
2025-08-26 22:59:11 -0300

    sla

--- adm/app/Http/Controllers/EventController.php
+++ adm/app/Http/Controllers/EventController.php
@@ -8,7 +8,7 @@ class EventController extends Controller
 {
     public function index() {
         return(
-            view('layout.app')
+            view('welcome')
         );
     }
 }
--- adm/resources/views/welcome.blade.php
+++ adm/resources/views/welcome.blade.php
@@ -9,7 +9,7 @@
     </div>
     <div id="mb-3">
         <label for="formFile" class="form-label">Imagem</label>
-        <input class="form-control" type="file" name="foto" id="formFile">
+        <input class="form-control form-control-sm" type="file" name="foto" id="formFile">
     </div>
     <div id="mb-3">
         <select class="form-select" aria-label="Default select example">
@@ -18,6 +18,10 @@
             <option value="2">Jogando</option>
             <option value="3">Em breve</option>
         </select>
+    </div>
+    <div class="mb-3">
+        <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
+        <textarea class="form-control form-control-sm" id="exampleFormControlTextarea1" rows="3"></textarea>
     </div>
     
     <button type="submit" class="btn btn-primary">Adicionar Jogo</button>

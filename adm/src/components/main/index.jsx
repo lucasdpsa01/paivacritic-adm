922a1915f18b87a59c2cc5e025152a233147fdf1
lucasdpsa01 <lucasdsa2801@gmail.com>
2025-10-26 21:27:11 -0300

    tudo certo!!! Agora é só adicionar os jogos

--- adm/src/components/main/index.jsx
+++ adm/src/components/main/index.jsx
@@ -1,39 +1,84 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState } from 'react';
+import { supabase } from '../../services/supabase';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
 import "./index.css"
 
 export default function Main() {
+    const [nome, setNome] = useState("")
+    const [descricao, setDescricao] = useState("")
+    const [situacao, setSituacao] = useState("")
+    const [nota, setNota] = useState("")
+
+    async function handleSubmit(e) {
+        e.preventDefault();
+
+        if (!nome || !descricao || !situacao || !nota) {
+            alert("Preencha todos os campos!")
+            return
+        };
+
+        const { data, error } = await supabase
+        .from("jogos")
+        .insert({
+            nome,
+            descricao,
+            situacao,
+            nota,
+        })
+
+        if (error) {
+            console.error(error)
+            alert("Erro ao enviar os dados!")
+        } else {
+            alert("Jogo adicionado com sucesso!")
+            console.log(data)
+            
+            setNome("");
+            setDescricao("");
+            setSituacao("");
+            setNota("");
+        }
+    }
 
     return (
         <div className='form-container'>
-            <form className='form-main'>
+            <form className='form-main' onSubmit={handleSubmit}>
                 <input
                     className='form-control form-control-sm'
                     aria-label=".form-control-sm example"
                     type="text"
                     placeholder="Nome do Jogo"
+                    onChange={(e) => setNome(e.target.value)}
+                    value={nome}
                     required
                 />
                 <textarea
-                    className='form-control'
+                    className='form-control form-control-sm'
                     id='floatingTextarea'
                     placeholder="Comente algo sobre o jogo"
+                    onChange={(e) => setDescricao(e.target.value)}
+                    value={descricao}
                     required
                 ></textarea>
-                <select className="form-select form-select-sm" aria-label="Small select example">
+                <select className="form-select form-select-sm" 
+                aria-label="Small select example"
+                onChange={(e) => setSituacao(e.target.value)}
+                value={situacao}
+                >
                     <option value="" disabled hidden required>Status</option>
-                    <option value="1">Zerei</option>
-                    <option value="2">Ainda estou jogando</option>
-                    <option value="3">Dropei</option>
-                    <option value="4">Em breve</option>
+                    <option value="Zerei">Zerei</option>
+                    <option value="Ainda estou jogando">Ainda estou jogando</option>
+                    <option value="Dropei">Dropei</option>
+                    <option value="Em breve">Em breve</option>
                 </select>
-                <select className="form-select form-select-sm" aria-label="Small select example">
-                    <option value="" disabled hidden required>Avaliação</option>
-                    <option value="1">Muito bom!</option>
-                    <option value="2">Bom</option>
-                    <option value="3">Médio</option>
-                    <option value="4">Não gostei</option>
-                </select>
+                <input 
+                type="number" 
+                onChange={(e) => setNota(e.target.value)}
+                value={nota}
+                min={1}
+                max={100}
+                />
 
                 <button type="submit" className='button'>Enviar</button>
             </form>
